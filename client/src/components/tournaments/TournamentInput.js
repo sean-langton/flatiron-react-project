3b741ec6e2d6e548717561d823c5569b73f7867e
github username <github email address>
2019-05-21 22:55:06 -0400

    Create Tournament form built

---
+++ client/src/components/tournaments/TournamentInput.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+export default class TournamentInput extends Component {
+
+  state = {
+    name: '',
+    date: '',
+    per_team: 0,
+    entry_code: ''
+  }
+
+  handleOnChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleOnSubmit = (event) => {
+    event.preventDefault();
+    this.props.addTournament(this.state)
+  }
+
+  render() {
+    return (
+      <Form>
+        <Form.Group controlId="formBasicTournament">
+          <Form.Label>Tournament Name</Form.Label><br />
+          <Form.Control type="text" name="name" value={this.state.name} onChange={this.handleOnChange} placeholder="Enter Tournament Name" /><br />
+          <Form.Label>Date</Form.Label><br />
+          <Form.Control type="date" name="date" value={this.state.date} onChange={this.handleOnChange}/><br />
+          <Form.Label>Per Team</Form.Label><br />
+          <Form.Control type="number" name="per_team" value={this.state.per_team} onChange={this.handleOnChange} placeholder="1-4" /><br />
+          <Form.Label>Entry Code (4 Characters)</Form.Label><br />
+          <Form.Control type="text" name="entry_code" value={this.state.entry_code} onChange={this.handleOnChange} placeholder="ABCD" /><br />
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form.Group>
+      </Form>
+    )
+  }
+}
--- client/src/containers/TournamentContainer.js
+++ client/src/containers/TournamentContainer.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react'
-
+import TournamentInput from '../components/tournaments/TournamentInput'
+import { connect } from 'react-redux'
 class TournamentContainer extends Component {
 
   render() {
     return (
       <div>
         Tournament Container
+        <TournamentInput addTournament={this.props.addTournament}/>
       </div>
     )
   }
 }
+const mapStateToProps = state => ({ tournaments: state.tournaments })
+
+const mapDispatchToProps = dispatch => ({
+  addTournament: tournament => dispatch({type: 'ADD_TOURNAMENT', tournament})
+})
 
 export default TournamentContainer

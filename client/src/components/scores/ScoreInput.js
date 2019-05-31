c7f50e587dbe05fe7ea4aed9eb91ca7c9d9f8976
github username <github email address>
2019-05-30 21:27:50 -0400

    playerID and holeId passing through to state correctly

--- client/src/components/scores/ScoreInput.js
+++ client/src/components/scores/ScoreInput.js
@@ -27,26 +27,32 @@ export default class ScoreInput extends Component {
   listPlayers = () => {
     return this.props.players.map((player,index) => {
         return (
-          <option key={index} id={player.id}>  {player.name} </option>
+          <option key={index} value={player.id}>  {player.name} </option>
         )
       })
   }
   listHoles = () => {
     return this.props.holes.map((hole,index) => {
         return (
-          <option key={index} id={hole.id}>  {hole.name} </option>
+          <option key={index} value={hole.id}>  {hole.name} </option>
         )
       })
+  }
+
+  handleOnChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
   }
   render() {
     return (
       <Form onSubmit={this.handleOnSubmit}>
         <Form.Label> Select Player </Form.Label>
-        <Form.Control as="select">
+        <Form.Control as="select" name="playerId" onChange={this.handleOnChange} >
           {this.listPlayers()}
         </Form.Control>
         <Form.Label> Select Hole </Form.Label>
-        <Form.Control as="select">
+        <Form.Control as="select" name="holeId" onChange={this.handleOnChange} >
           {this.listHoles()}
         </Form.Control>
         <Button variant="primary" type="submit">
